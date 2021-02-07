7e1d0855f3af3ea797ae3ef5375c288c9d1c00b8
ayumitamai97 <ayumitamai97@gmail.com>
2021-02-07 16:43:05 +0900

    Update Post.title nullability

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,7 +3,7 @@ module Types
     description 'Post'
 
     field :id, ID, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :content, String, null: true
   end
 end
